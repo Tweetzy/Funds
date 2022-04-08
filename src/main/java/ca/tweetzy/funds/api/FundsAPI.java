4f71abbd5bbf50fdf967136288975dd7ef86744e
Kiran Hart <kiran.hart@live.com>
2022-04-08 19:17:41 -0400

    todo add vault support

---
+++ src/main/java/ca/tweetzy/funds/api/FundsAPI.java
@@ -0,0 +1,10 @@
+package ca.tweetzy.funds.api;
+
+/**
+ * Date Created: April 08 2022
+ * Time Created: 7:13 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class FundsAPI {
+}
--- src/main/java/ca/tweetzy/funds/settings/Settings.java
+++ src/main/java/ca/tweetzy/funds/settings/Settings.java
@@ -2,6 +2,7 @@ package ca.tweetzy.funds.settings;
 
 import ca.tweetzy.funds.Funds;
 import ca.tweetzy.rose.configuration.Config;
+import ca.tweetzy.rose.configuration.ConfigSetting;
 
 /**
  * Date Created: April 08 2022
@@ -12,6 +13,8 @@ import ca.tweetzy.rose.configuration.Config;
 public final class Settings {
 
 	private static final Config config = Funds.getInstance().getCoreConfig();
+
+	public static final ConfigSetting LANG = new ConfigSetting(config, "language", "en_US", "The default language for the plugin");
 
 	public static void setup() {
 		config.load();
