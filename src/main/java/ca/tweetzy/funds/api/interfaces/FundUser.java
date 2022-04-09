f57c313743fdb404734bb0ebb1f1d89095ddc9a3
Kiran Hart <kiran.hart@live.com>
2022-04-08 23:19:40 -0400

    basic FundUser

---
+++ src/main/java/ca/tweetzy/funds/api/interfaces/FundUser.java
@@ -0,0 +1,15 @@
+package ca.tweetzy.funds.api.interfaces;
+
+import java.util.UUID;
+
+/**
+ * Date Created: April 08 2022
+ * Time Created: 11:07 p.m.
+ *
+ * @author Kiran Hart
+ */
+public interface FundUser {
+
+	UUID getUUID();
+
+}
