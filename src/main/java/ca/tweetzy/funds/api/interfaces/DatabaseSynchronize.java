a64a93269bbacaf898e6332b783adca07f93470e
Kiran Hart <kiran.hart@live.com>
2022-04-11 01:19:43 -0400

    create DatabaseSynchronize interface
    Took 9 seconds

---
+++ src/main/java/ca/tweetzy/funds/api/interfaces/DatabaseSynchronize.java
@@ -0,0 +1,12 @@
+package ca.tweetzy.funds.api.interfaces;
+
+/**
+ * Date Created: April 11 2022
+ * Time Created: 12:55 a.m.
+ *
+ * @author Kiran Hart
+ */
+public interface DatabaseSynchronize {
+
+	void sync();
+}
