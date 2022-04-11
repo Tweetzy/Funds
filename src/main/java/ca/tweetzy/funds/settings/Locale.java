9b46b63e7e179938b769bd9e6f9f2b43d88654e0
Kiran Hart <kiran.hart@live.com>
2022-04-10 23:14:07 -0400

    added locale
    Took 9 seconds

---
+++ src/main/java/ca/tweetzy/funds/settings/Locale.java
@@ -0,0 +1,26 @@
+package ca.tweetzy.funds.settings;
+
+import ca.tweetzy.funds.Funds;
+import ca.tweetzy.rose.configuration.Config;
+import ca.tweetzy.rose.configuration.ConfigSetting;
+
+/**
+ * Date Created: April 10 2022
+ * Time Created: 4:22 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class Locale {
+
+	private static final Config config = Funds.getLangConfig();
+
+	public static final ConfigSetting CURRENCY_ALREADY_EXISTS = new ConfigSetting(config, "Currency Already Exists", "&cA currency by that id already exists!");
+	public static final ConfigSetting CURRENCY_CREATE_ERROR = new ConfigSetting(config, "Currency Creation Failed", "&cSomething went wrong while creating that currency");
+	public static final ConfigSetting CURRENCY_CREATED = new ConfigSetting(config, "Currency Created", "&aCreated a new currency named&F: &e%currency_name%");
+
+	public static void setup() {
+		config.load();
+		config.setAutosave(true).setAutosave(true);
+		config.saveChanges();
+	}
+}
