073f5c3290f69745192d3a583a45a8042520593e
Kiran Hart <kiran.hart@live.com>
2022-04-12 00:05:25 -0400

    access (join event) listener
    Took 27 minutes

---
+++ src/main/java/ca/tweetzy/funds/listeners/AccessListeners.java
@@ -0,0 +1,34 @@
+package ca.tweetzy.funds.listeners;
+
+import ca.tweetzy.funds.Funds;
+import ca.tweetzy.funds.api.interfaces.Account;
+import ca.tweetzy.funds.impl.FundAccount;
+import ca.tweetzy.rose.utils.Common;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * Date Created: April 11 2022
+ * Time Created: 11:03 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class AccessListeners implements Listener {
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerJoin(final PlayerJoinEvent event) {
+		// create user account if not exists
+		final Player player = event.getPlayer();
+
+		final Account playerAccount = Funds.getAccountManager().getAccount(player);
+		if (playerAccount == null) {
+			Funds.getAccountManager().createAccount(new FundAccount(player.getUniqueId()), (created, account) -> {
+				if (created)
+					Common.log(String.format("&fCreated user account for player &e%s", player.getName()));
+			});
+		}
+	}
+}
