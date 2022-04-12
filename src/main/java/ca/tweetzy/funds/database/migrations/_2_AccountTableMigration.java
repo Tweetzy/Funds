dfe1ba172c25bbc8d2b79a156ed6f7e1e3d6d29c
Kiran Hart <kiran.hart@live.com>
2022-04-11 22:43:19 -0400

    account migration
    Took 25 minutes

---
+++ src/main/java/ca/tweetzy/funds/database/migrations/_2_AccountTableMigration.java
@@ -0,0 +1,32 @@
+package ca.tweetzy.funds.database.migrations;
+
+import ca.tweetzy.rose.database.DataMigration;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Date Created: April 11 2022
+ * Time Created: 9:19 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class _2_AccountTableMigration extends DataMigration {
+
+	public _2_AccountTableMigration() {
+		super(2);
+	}
+
+	@Override
+	public void migrate(Connection connection, String tablePrefix) throws SQLException {
+		try (Statement statement = connection.createStatement()) {
+			statement.execute("CREATE TABLE " + tablePrefix + "account (" +
+					"id VARCHAR(36) PRIMARY KEY, " +
+					"bal_top_blocked BOOLEAN NOT NULL, " +
+					"currencies TEXT NOT NULL, " +
+					"created_at LONG NOT NULL" +
+					")");
+		}
+	}
+}
