39b7347990c8d7b435972702ff1f45a0469e2769
Kiran Hart <kiran.hart@live.com>
2022-04-12 22:58:43 -0400

    finished the main vault hook method implementations, ill get around to finishing the username method impls
    Took 39 seconds

--- src/main/java/ca/tweetzy/funds/hooks/VaultHook.java
+++ src/main/java/ca/tweetzy/funds/hooks/VaultHook.java
@@ -1,12 +1,17 @@
 package ca.tweetzy.funds.hooks;
 
 import ca.tweetzy.funds.Funds;
+import ca.tweetzy.funds.api.interfaces.Account;
+import ca.tweetzy.funds.api.interfaces.Currency;
+import ca.tweetzy.funds.impl.FundAccount;
+import ca.tweetzy.rose.utils.Common;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.ServicePriority;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -19,6 +24,7 @@ public final class VaultHook implements Economy {
 
 	public VaultHook() {
 		Bukkit.getServer().getServicesManager().register(Economy.class, this, Funds.getInstance(), ServicePriority.Highest);
+		Common.log("&aHooked into Vault");
 	}
 
 	@Override
@@ -43,86 +49,156 @@ public final class VaultHook implements Economy {
 
 	@Override
 	public String format(double amount) {
-		return null;
+		return String.format("%s %s", String.format("%,.2f", amount), amount == 1D ? Funds.getCurrencyManager().getCurrency("coins").getSingularFormat() : Funds.getCurrencyManager().getCurrency("coins").getPluralFormat());
 	}
 
 	@Override
 	public String currencyNamePlural() {
-		return null;
+		return Funds.getCurrencyManager().getCurrency("coins").getPluralFormat();
 	}
 
 	@Override
 	public String currencyNameSingular() {
-		return null;
-	}
-
-	@Override
-	public boolean hasAccount(String playerName) {
-		return false;
+		return Funds.getCurrencyManager().getCurrency("coins").getSingularFormat();
 	}
 
 	@Override
 	public boolean hasAccount(OfflinePlayer player) {
-		return false;
-	}
-
-	@Override
-	public boolean hasAccount(String playerName, String worldName) {
-		return false;
+		return Funds.getAccountManager().getAccount(player) != null;
 	}
 
 	@Override
 	public boolean hasAccount(OfflinePlayer player, String worldName) {
-		return false;
-	}
-
-	@Override
-	public double getBalance(String playerName) {
-		return 0;
+		return hasAccount(player);
 	}
 
 	@Override
 	public double getBalance(OfflinePlayer player) {
-		return 0;
-	}
-
-	@Override
-	public double getBalance(String playerName, String world) {
-		return 0;
+		final Account account = Funds.getAccountManager().getAccount(player);
+		final Currency currency = Funds.getCurrencyManager().getCurrency("coins");
+		return account != null && currency != null ? account.getCurrencies().get(currency) : 0D;
 	}
 
 	@Override
 	public double getBalance(OfflinePlayer player, String world) {
-		return 0;
-	}
-
-	@Override
-	public boolean has(String playerName, double amount) {
-		return false;
+		return getBalance(player);
 	}
 
 	@Override
 	public boolean has(OfflinePlayer player, double amount) {
-		return false;
-	}
-
-	@Override
-	public boolean has(String playerName, String worldName, double amount) {
-		return false;
+		return getBalance(player) >= amount;
 	}
 
 	@Override
 	public boolean has(OfflinePlayer player, String worldName, double amount) {
-		return false;
-	}
-
-	@Override
-	public EconomyResponse withdrawPlayer(String playerName, double amount) {
-		return null;
+		return getBalance(player) >= amount;
 	}
 
 	@Override
 	public EconomyResponse withdrawPlayer(OfflinePlayer player, double amount) {
+		final Account account = Funds.getAccountManager().getAccount(player);
+		final Currency currency = Funds.getCurrencyManager().getCurrency("coins");
+
+		if (account == null || currency == null)
+			return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Funds vault currency not set or user account not found!");
+
+		if (account.getCurrencies().get(currency) < amount) {
+			return new EconomyResponse(0D, account.getCurrencies().get(currency), EconomyResponse.ResponseType.FAILURE, null);
+		}
+
+		final double currentBalance = account.getCurrencies().get(currency);
+		account.getCurrencies().put(currency, currentBalance - amount);
+		account.sync();
+
+		return new EconomyResponse(amount, account.getCurrencies().get(currency), EconomyResponse.ResponseType.SUCCESS, null);
+	}
+
+	@Override
+	public EconomyResponse withdrawPlayer(OfflinePlayer player, String worldName, double amount) {
+		return withdrawPlayer(player, amount);
+	}
+
+	@Override
+	public EconomyResponse depositPlayer(OfflinePlayer player, double amount) {
+		final Account account = Funds.getAccountManager().getAccount(player);
+		final Currency currency = Funds.getCurrencyManager().getCurrency("coins");
+
+		if (account == null || currency == null)
+			return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Funds vault currency not set or user account not found!");
+
+
+		final double currentBalance = account.getCurrencies().get(currency);
+		account.getCurrencies().put(currency, currentBalance + amount);
+		account.sync();
+
+		return new EconomyResponse(amount, account.getCurrencies().get(currency), EconomyResponse.ResponseType.SUCCESS, null);
+	}
+
+	@Override
+	public EconomyResponse depositPlayer(OfflinePlayer player, String worldName, double amount) {
+		return depositPlayer(player, amount);
+	}
+
+	@Override
+	public boolean createPlayerAccount(OfflinePlayer player) {
+		Funds.getAccountManager().createAccount(new FundAccount(player), (created, account) -> {
+			if (created)
+				Common.log(String.format("&fCreated user account for player &e%s", player.getName()));
+		});
+
+		return true;
+	}
+
+
+	@Override
+	public boolean createPlayerAccount(OfflinePlayer player, String worldName) {
+		return createPlayerAccount(player);
+	}
+
+	/*
+	===========================================================================
+	============= oMg GoD wHy U gOtTa UsE nAmEs InStEaD oF UuIdS ==============
+	===========================================================================
+
+							  THE SECTION OF SHAME
+
+	===========================================================================
+	 */
+
+	@Override
+	public boolean hasAccount(String playerName) {
+		return Funds.getAccountManager().getAccount(playerName) != null;
+	}
+
+	@Override
+	public boolean hasAccount(String playerName, String worldName) {
+		return hasAccount(playerName);
+	}
+
+	@Override
+	public double getBalance(String playerName) {
+		final Account account = Funds.getAccountManager().getAccount(playerName);
+		final Currency currency = Funds.getCurrencyManager().getCurrency("coins");
+		return account != null && currency != null ? account.getCurrencies().get(currency) : 0D;
+	}
+
+	@Override
+	public double getBalance(String playerName, String world) {
+		return getBalance(playerName);
+	}
+
+	@Override
+	public boolean has(String playerName, double amount) {
+		return getBalance(playerName) >= amount;
+	}
+
+	@Override
+	public boolean has(String playerName, String worldName, double amount) {
+		return getBalance(playerName) >= amount;
+	}
+
+	@Override
+	public EconomyResponse withdrawPlayer(String playerName, double amount) {
 		return null;
 	}
 
@@ -132,17 +208,7 @@ public final class VaultHook implements Economy {
 	}
 
 	@Override
-	public EconomyResponse withdrawPlayer(OfflinePlayer player, String worldName, double amount) {
-		return null;
-	}
-
-	@Override
 	public EconomyResponse depositPlayer(String playerName, double amount) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse depositPlayer(OfflinePlayer player, double amount) {
 		return null;
 	}
 
@@ -152,68 +218,8 @@ public final class VaultHook implements Economy {
 	}
 
 	@Override
-	public EconomyResponse depositPlayer(OfflinePlayer player, String worldName, double amount) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse createBank(String name, String player) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse createBank(String name, OfflinePlayer player) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse deleteBank(String name) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse bankBalance(String name) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse bankHas(String name, double amount) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse bankWithdraw(String name, double amount) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse bankDeposit(String name, double amount) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse isBankOwner(String name, String playerName) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse isBankOwner(String name, OfflinePlayer player) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse isBankMember(String name, String playerName) {
-		return null;
-	}
-
-	@Override
-	public EconomyResponse isBankMember(String name, OfflinePlayer player) {
-		return null;
-	}
-
-	@Override
-	public List<String> getBanks() {
-		return null;
+	public boolean createPlayerAccount(String playerName, String worldName) {
+		return false;
 	}
 
 	@Override
@@ -221,18 +227,69 @@ public final class VaultHook implements Economy {
 		return false;
 	}
 
+
+	/*
+	=========================== BANK SHIT ===========================
+	 */
+
 	@Override
-	public boolean createPlayerAccount(OfflinePlayer player) {
-		return false;
+	public EconomyResponse createBank(String name, String player) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
 	}
 
 	@Override
-	public boolean createPlayerAccount(String playerName, String worldName) {
-		return false;
+	public EconomyResponse createBank(String name, OfflinePlayer player) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+
 	}
 
 	@Override
-	public boolean createPlayerAccount(OfflinePlayer player, String worldName) {
-		return false;
+	public EconomyResponse deleteBank(String name) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+	}
+
+	@Override
+	public EconomyResponse bankBalance(String name) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+	}
+
+	@Override
+	public EconomyResponse bankHas(String name, double amount) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+	}
+
+	@Override
+	public EconomyResponse bankWithdraw(String name, double amount) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+	}
+
+	@Override
+	public EconomyResponse bankDeposit(String name, double amount) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+	}
+
+	@Override
+	public EconomyResponse isBankOwner(String name, String playerName) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+	}
+
+	@Override
+	public EconomyResponse isBankOwner(String name, OfflinePlayer player) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+	}
+
+	@Override
+	public EconomyResponse isBankMember(String name, String playerName) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+	}
+
+	@Override
+	public EconomyResponse isBankMember(String name, OfflinePlayer player) {
+		return new EconomyResponse(0D, 0D, EconomyResponse.ResponseType.NOT_IMPLEMENTED, "Not currently supported by Funds");
+	}
+
+	@Override
+	public List<String> getBanks() {
+		return Collections.emptyList();
 	}
 }
