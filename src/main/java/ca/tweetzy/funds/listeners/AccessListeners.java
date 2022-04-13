624d8a6548e66056cefebed0a1ea533b9169b317
Kiran Hart <kiran.hart@live.com>
2022-04-12 22:58:59 -0400

    update player name on join if changed
    Took 15 seconds

--- src/main/java/ca/tweetzy/funds/listeners/AccessListeners.java
+++ src/main/java/ca/tweetzy/funds/listeners/AccessListeners.java
@@ -24,11 +24,21 @@ public final class AccessListeners implements Listener {
 		final Player player = event.getPlayer();
 
 		final Account playerAccount = Funds.getAccountManager().getAccount(player);
+
 		if (playerAccount == null) {
 			Funds.getAccountManager().createAccount(new FundAccount(player.getUniqueId()), (created, account) -> {
 				if (created)
 					Common.log(String.format("&fCreated user account for player &e%s", player.getName()));
 			});
+
+			return;
+		}
+
+		// check and update player name if diff
+		if (!playerAccount.getName().equalsIgnoreCase(player.getName())) {
+			Common.log(String.format("&fUser &e%s &fhas changed their name to &e%s&f, updating account info.", playerAccount.getName(), player.getName()));
+			playerAccount.setName(player.getName());
+			playerAccount.sync();
 		}
 	}
 }
