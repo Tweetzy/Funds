3577b5b57635c05dd79938aea0ca5a183f2a4c9b
Kiran Hart <kiran.hart@live.com>
2022-04-14 00:01:35 -0400

    return AccountsAPI instance in funds api
    Took 12 seconds

--- src/main/java/ca/tweetzy/funds/api/FundsAPI.java
+++ src/main/java/ca/tweetzy/funds/api/FundsAPI.java
@@ -1,10 +1,30 @@
 package ca.tweetzy.funds.api;
 
+import ca.tweetzy.funds.api.interfaces.AccountAPI;
+import ca.tweetzy.funds.api.interfaces.CurrencyAPI;
+import ca.tweetzy.funds.api.interfaces.FundAPI;
+import ca.tweetzy.funds.api.interfaces.TransactionAPI;
+
 /**
- * Date Created: April 08 2022
- * Time Created: 7:13 p.m.
+ * Date Created: April 13 2022
+ * Time Created: 11:47 p.m.
  *
  * @author Kiran Hart
  */
-public final class FundsAPI {
+public final class FundsAPI implements FundAPI {
+
+	@Override
+	public AccountAPI getAccountAPI() {
+		return AccountsAPI.getInstance();
+	}
+
+	@Override
+	public CurrencyAPI getCurrencyAPI() {
+		return null;
+	}
+
+	@Override
+	public TransactionAPI getTransactionAPI() {
+		return null;
+	}
 }
