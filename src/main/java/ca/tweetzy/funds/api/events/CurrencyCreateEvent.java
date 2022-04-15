392336767d48c79d09ed205ee03710667c9f043a
Kiran Hart <kiran.hart@live.com>
2022-04-15 13:31:06 -0400

    custom event files
    Took 7 minutes

---
+++ src/main/java/ca/tweetzy/funds/api/events/CurrencyCreateEvent.java
@@ -0,0 +1,10 @@
+package ca.tweetzy.funds.api.events;
+
+/**
+ * Date Created: April 15 2022
+ * Time Created: 1:15 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class CurrencyCreateEvent {
+}
---
+++ src/main/java/ca/tweetzy/funds/api/events/CurrencyEditEvent.java
@@ -0,0 +1,10 @@
+package ca.tweetzy.funds.api.events;
+
+/**
+ * Date Created: April 15 2022
+ * Time Created: 1:16 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class CurrencyEditEvent {
+}
