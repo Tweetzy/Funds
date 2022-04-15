936d9db5e4ce0c8b24c3cd42ce79a7313b6063f3
Kiran Hart <kiran.hart@live.com>
2022-04-15 13:31:14 -0400

    support link cmd
    Took 7 seconds

---
+++ src/main/java/ca/tweetzy/funds/commands/SupportCommand.java
@@ -0,0 +1,55 @@
+package ca.tweetzy.funds.commands;
+
+import ca.tweetzy.rose.command.AllowedExecutor;
+import ca.tweetzy.rose.command.Command;
+import ca.tweetzy.rose.command.ReturnType;
+import ca.tweetzy.rose.utils.ChatUtil;
+import ca.tweetzy.rose.utils.Common;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+/**
+ * Date Created: April 15 2022
+ * Time Created: 1:25 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class SupportCommand extends Command {
+
+	public SupportCommand() {
+		super(AllowedExecutor.PLAYER, "support");
+	}
+
+	@Override
+	protected ReturnType execute(CommandSender sender, String... args) {
+		Common.tellNoPrefix(sender,
+				"&8&m-----------------------------------------------------",
+				"",
+				ChatUtil.centerMessage("&E&lFunds Support"),
+				ChatUtil.centerMessage("&bhttps://discord.tweetzy.ca"),
+				"&8&m-----------------------------------------------------"
+		);
+		return ReturnType.SUCCESS;
+	}
+
+	@Override
+	protected List<String> tab(CommandSender sender, String... args) {
+		return null;
+	}
+
+	@Override
+	public String getPermissionNode() {
+		return "funds.cmd.support";
+	}
+
+	@Override
+	public String getSyntax() {
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return null;
+	}
+}
